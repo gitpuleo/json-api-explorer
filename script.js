9e40dc923c2bed819712082ad97ebd8643fb4739
nfp <nfpuleo@gmail.com>
2025-07-12 16:49:53 -0500

    Finally got posts to submit with fetch!

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
-// so much empty
+fetch("https://jsonplaceholder.typicode.com/posts")
+.then(function(response) {
+    return response.json();
+})
+.then(function(json) {
+    const postlist = document.getElementById("postList");
+    const ul = document.createElement("ul");
+    let jsonPost = json;
+
+    jsonPost.forEach(post => {
+    const li = document.createElement("li");
+    li.innerHTML = `<p>${post.title}</p> \n ${post.body}`
+    ul.appendChild(li);
+});
+postlist.appendChild(ul);
+})
+.catch(function(error) {
+    console.error("Error fetching data:", error);
+});
+
+let postForm = document.getElementById("postForm");
+let titleInput = document.getElementById("titleInput");
+let bodyInput = document.getElementById("bodyInput");
+let formError = document.getElementById("formError");
+let formSuccess = document.getElementById("formSuccess");
+
+
+postForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let userInputPost = {
+        title: titleInput.value,
+        body: bodyInput.value,
+    };
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userInputPost)
+    })
+    .then(response =>{
+        return response.json();
+    })
+    .then(data => {
+        formError.textContent = "";
+        formSuccess.innerHTML = `<p>Submitted</p>`;
+    })
+
+})
+
